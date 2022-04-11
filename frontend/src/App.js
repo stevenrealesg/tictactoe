6392134855adcf7c3d2c1e28f81325c896ca49d9
Steven Reales <stevenreales@gmail.com>
2022-04-10 20:03:30 -0500

    operation of the game

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,62 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import Board from './components/Board'
+import Header from './components/StatusPlayer';
 
 function App() {
+  const [data, setData] = useState(['', '', '', '', '', '', '', '', ''])
+  const [playerX, setPlayerX] = useState(true)
+  const [countMovs, setCountMovs] = useState(0)
+  const [winner, setWinner] = useState(null)
+
+  const verifyWinner = (infoBoard) => {
+    const casesToWin = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [6, 4, 2]
+    ]
+    for (let i = 0; i < casesToWin.length; i++) {
+      const [c1, c2, c3] = casesToWin[i]
+      if (infoBoard[c1] && infoBoard[c1] === infoBoard[c2] && infoBoard[c1] === infoBoard[c3]) {
+        return {
+          won: true,
+          player: infoBoard[c1]
+        }
+      }
+    }
+
+    return {
+      won: false
+    }
+  }
+
+  const handleClickRetry = () => {
+    setData((['', '', '', '', '', '', '', '', '']))
+    setPlayerX(true)
+    setCountMovs(0)
+    setWinner(null)
+  }
+
+  useEffect(() => {
+    setCountMovs(countMovs + 1)
+    const status = verifyWinner(data)
+    if (status.won || countMovs == 9) {
+      setWinner({ ...status, date: new Date })
+    }
+  }, [data])
+
   return (
     <div className="App">
-      <div className='header'> Turno para: X</div>
+      <div className='header'>
+        <Header player={playerX} winner={winner} handleClickRetry={handleClickRetry} />
+      </div>
       <div className='game'>
-        <Board />
+        <Board data={data} setData={setData} playerX={playerX} setPlayerX={setPlayerX} winner={winner} />
       </div>
     </div>
   );
--- frontend/src/components/Board.jsx
+++ frontend/src/components/Board.jsx
@@ -1,21 +1,26 @@
 import Cell from "./Cell";
 
-function Board() {
+function Board({ data, setData, playerX, setPlayerX, winner }) {
     const handleClick = (index) => {
-        console.log("presionando", index)
+        if (!data[index] && !winner) {
+            let newData = [...data]
+            newData[index] = playerX ? 'X' : 'O'
+            setData(newData)
+            setPlayerX(!playerX)
+        }
     }
 
     return (
         <div className="board">
-            <Cell index={0} handleClick={handleClick}/>
-            <Cell index={1} handleClick={handleClick}/>
-            <Cell index={2} handleClick={handleClick}/>
-            <Cell index={3} handleClick={handleClick}/>
-            <Cell index={4} handleClick={handleClick}/>
-            <Cell index={5} handleClick={handleClick}/>
-            <Cell index={6} handleClick={handleClick}/>
-            <Cell index={7} handleClick={handleClick}/>
-            <Cell index={8} handleClick={handleClick}/>
+            <Cell index={0} handleClick={handleClick} value={data[0]} />
+            <Cell index={1} handleClick={handleClick} value={data[1]} />
+            <Cell index={2} handleClick={handleClick} value={data[2]} />
+            <Cell index={3} handleClick={handleClick} value={data[3]} />
+            <Cell index={4} handleClick={handleClick} value={data[4]} />
+            <Cell index={5} handleClick={handleClick} value={data[5]} />
+            <Cell index={6} handleClick={handleClick} value={data[6]} />
+            <Cell index={7} handleClick={handleClick} value={data[7]} />
+            <Cell index={8} handleClick={handleClick} value={data[8]} />
         </div>
     );
 }
